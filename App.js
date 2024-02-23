b015839299263e043c52e0e61b7970ca9ef076d4
mListek <michal.listex@gmail.com>
2024-02-23 14:28:54 +0100

    added content and styling

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text } from "react-native";
+import TodoList from "./components/todoList";
+import colors from "./global/colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Text style={styles.headerText}>To do:</Text>
+      <TodoList />
+      <Text style={styles.headerText}>Done:</Text>
     </View>
   );
 }
@@ -13,8 +17,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.background,
+    paddingTop: 70,
+  },
+  headerText: {
+    color: colors.neonOrange,
+    fontWeight: "bold",
+    fontSize: 30,
+    margin: 15,
   },
 });
