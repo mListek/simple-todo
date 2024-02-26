4fb8db21c6d141a4890d7c8750b0680f0b781ed6
mListek <michal.listex@gmail.com>
2024-02-26 14:58:25 +0100

    created component

--- components/todoSection.js
+++ components/todoSection.js
@@ -1,10 +1,58 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TextInput } from "react-native";
+import { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+
 import colors from "../global/colors";
 
 const TodoSection = ({ item }) => {
+  const [todoContent, setTodoContent] = useState(item.content);
+  const [isDone, setIsDone] = useState(false);
+
+  const handleIsDone = () => {
+    setIsDone((prev) => !prev);
+    setTimeout(() => {
+      // delete current item
+      setTodoContent(""); // temporary
+    }, 1500);
+  };
+
+  const handleTextChange = (enteredValue) => {
+    setTodoContent(enteredValue);
+  };
+
+  const handleSaveChanges = () => {
+    return;
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{item.content}</Text>
+      <View style={styles.buttonContainer}>
+        {!isDone && (
+          <Ionicons
+            name="ellipse-outline"
+            size={30}
+            color={colors.neonOrange}
+            onPress={handleIsDone}
+          />
+        )}
+        {isDone && (
+          <Ionicons
+            name="checkmark-circle-outline"
+            size={28}
+            color={colors.neonOrange}
+            onPress={handleIsDone}
+          />
+        )}
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          keyboardAppearance="dark"
+          value={todoContent}
+          onChangeText={handleTextChange}
+          onEndEditing={handleSaveChanges}
+        />
+      </View>
     </View>
   );
 };
@@ -13,12 +61,17 @@ export default TodoSection;
 
 const styles = StyleSheet.create({
   container: {
-    margin: 5,
+    padding: 2,
     borderBottomColor: colors.borderColor,
     borderBottomWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
   },
-  text: {
+  buttonContainer: {
+    marginRight: 8,
+  },
+  textInput: {
     color: colors.lightText,
-    fontSize: 16,
+    fontSize: 28,
   },
 });
