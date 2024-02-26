ae9a2f7fbef81072bf0e7a2bf66e0eef49f5eb28
mListek <michal.listex@gmail.com>
2024-02-26 20:30:58 +0100

    added new todo button

--- components/todoList.js
+++ components/todoList.js
@@ -1,9 +1,11 @@
-import { FlatList } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 import { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import Todo from "../model/todo";
 import TodoSection from "./todoSection";
+import Button from "../ui/button";
+import colors from "../global/colors";
 
 const renderTodoItem = (itemData) => {
   return <TodoSection item={itemData.item} />;
@@ -32,13 +34,13 @@ const TodoList = () => {
     saveData();
   }, [todoList]);
 
-  const addTodo = (todoContent) => {
-    const id = new Date().toString + Math.random().toString();
+  const addTodo = () => {
+    const id = Date.now().toString() + Math.random().toString();
     const date = new Date();
     const dateString = `${date.getFullYear()}.${date.getMonth()}.${date.getDate()} ${date.getHours()}:${
       date.getMinutes
     }`;
-    setTodoList((prev) => [...prev, new Todo(id, todoContent, dateString)]);
+    setTodoList((prev) => [...prev, new Todo(id, "", dateString)]);
   };
 
   const updateTodo = (id, content) => {
@@ -51,16 +53,37 @@ const TodoList = () => {
   };
 
   const deleteTodo = (id) => {
+    console.log("Deleting todo: " + id);
     setTodoList((prev) => prev.filter((todo) => todo.id !== id));
   };
 
   return (
-    <FlatList
-      data={todoList}
-      keyExtractor={(item) => item.id}
-      renderItem={renderTodoItem}
-    />
+    <>
+      <FlatList
+        data={todoList}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TodoSection item={item} deleteTodo={deleteTodo} />
+        )}
+      />
+      <View style={styles.buttonContainer}>
+        <Button
+          name="add-circle"
+          color={colors.neonOrange}
+          size={30}
+          onPress={addTodo}
+          label="New Task"
+        />
+      </View>
+    </>
   );
 };
 
 export default TodoList;
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    borderColor: "white",
+    marginBottom: 45,
+  },
+});
